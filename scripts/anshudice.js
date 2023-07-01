a70df3efbef5cf7d496dfdfb8daa873cfd0defb0
Taurus-II <65988722+Taurus-II@users.noreply.github.com>
2023-07-01 11:59:53 -0400

    Add files via upload

--- scripts/anshudice.js
+++ scripts/anshudice.js
@@ -15,11 +15,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
        "7",
        "8",
        "8",
-       "modules/anshudice/images/lunar_10.png"
+       "☽"
      ],
-	 bumpMaps: [,,,,,,,,,
-		"modules/anshudice/images/lunar_10_BUMP.png"
-	 ],
+	 font: "Examorphous",
 	 values: {min:1,max:10},
      system: "Exalted - Lunar" 
    },"d10");
@@ -38,13 +36,32 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
        "7",
        "8",
        "8",
-       "anshudice/images/solar_10.png"
+       "☀"
      ],
-	 bumpMaps: [,,,,,,,,,
-		"modules/anshudice/images/solar_10_BUMP.png"
-	 ],
+	 font: "Examorphous",
 	 values: {min:1,max:10},
      system: "Exalted - Solar" 
+   },"d10");
+   
+  dice3d.addSystem({ id: "Exalted - Solar", name: "Exalted - Little Dragon" }, "default");
+
+   dice3d.addDicePreset({
+     type: "d10",
+	      labels: [
+       "1",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "8",
+       "⚖"
+     ],
+	 font: "Examorphous",
+	 values: {min:1,max:10},
+     system: "Exalted - Little Dragon" 
    },"d10");
 	
 });
